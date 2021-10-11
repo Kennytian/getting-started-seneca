e60bdeacb50a265dc9f2cfc33dfb42df9fa78b8b
Kenny <kt.tian@gmail.com>
2021-10-11 11:42:29 +0800

    Update sum-product.js

--- sum-product.js
+++ sum-product.js
@@ -1,12 +1,23 @@
-const seneca = require('seneca')();
+const Seneca = require('seneca');
+const seneca = new Seneca();
 
-seneca.add('role:math, cmd:sum', (msg, reply) => {
-  reply(null, { answer: ( msg.left + msg.right )})
+seneca.add('role:math, cmd:sum', (msg, resp) => {
+    resp(null, {answer: msg.left + msg.right});
 });
 
-seneca.add('role:math, cmd:product', (msg, reply) => {
-  reply(null, { answer: ( msg.left * msg.right )})
+seneca.add('role:math, cmd:product', (msg, resp) => {
+    resp(null, {answer: msg.left * msg.right});
 });
 
-seneca.act({role: 'math', cmd: 'sum', left: 1, right: 2}, console.log)
-      .act({role: 'math', cmd: 'product', left: 3, right: 4}, console.log)
+seneca.act({role: 'math', cmd: 'sum', left: 1, right: 2}, (err, result) => {
+    if (err) {
+        return console.error(err)
+    }
+    console.log(result)
+})
+.act({role: 'math', cmd: 'product', left: 3, right: 4}, (err, result) => {
+    if (err) {
+        return console.error(err)
+    }
+    console.log(result)
+});
