b9dbdc1c3075ccfa18d87ac1c8b70d1da19f8aaa
Kenny <kt.tian@gmail.com>
2021-10-11 17:08:16 +0800

    Update math-pin-client.js

--- math-pin-client.js
+++ math-pin-client.js
@@ -1,14 +1,16 @@
-require('seneca')()
+const Seneca = require('seneca');
+const seneca = new Seneca();
 
-  // 本地模式
-  .add('say:hello', function (msg, respond){ respond(null, {text: "Hi!"}) })
+seneca
+    // 本地模式
+    .add('say:hello', (msg, reply) => reply(null, {text: 'Hi!'}))
 
-  // 发送 role:math 模式至服务
-  // 注意：必须匹配服务端
-  .client({ type: 'tcp', pin: 'role:math' })
+    // 发送 role:math 模式至服务
+    // 注意：必须匹配服务端
+    .client({ type: 'tcp', pin: 'role:math' })
 
-  // 远程操作
-  .act('role:math,cmd:sum,left:1,right:2',console.log)
+    // 远程操作
+    .act('role:math,cmd:sum,left:1,right:2', console.log)
 
-  // 本地操作
-  .act('say:hello',console.log)
+    // 本地操作
+    .act('say:hello', console.log);
