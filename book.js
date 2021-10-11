6868bce003fd2838241365e96c44dd7437ec76b5
Kenny <kt.tian@gmail.com>
2021-10-11 18:14:38 +0800

    Update book.js

--- book.js
+++ book.js
@@ -1,4 +1,7 @@
-const seneca = require('seneca')();
+const Seneca = require('seneca');
+const seneca = new Seneca();
+
+//需要 npm i seneca-entity -S
 seneca.use('basic').use('entity');
 
 const book = seneca.make('book');
@@ -6,4 +9,6 @@ book.title = 'Action in Seneca';
 book.price = 9.99;
 
 // 发送 role:entity,cmd:save,name:book 消息
-book.save$( console.log );
+book.save$((props, {id, title, price}) => {
+    console.log('cb ====', id, title, price);
+});
