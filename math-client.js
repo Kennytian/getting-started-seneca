4db557fa8db4af2a40048d18517a3c1d10be9a45
Kenny <kt.tian@gmail.com>
2021-10-11 17:44:40 +0800

    Update math-client.js

--- math-client.js
+++ math-client.js
@@ -1,3 +1,11 @@
-require('seneca')()
-  .client()
-  .act('role:math,cmd:sum,left:1,right:2',console.log)
+const Seneca = require('seneca');
+const seneca = new Seneca();
+
+function output(err, result) {
+    if (err) {
+        return console.error(err);
+    }
+    console.log(result);
+}
+
+seneca.client().act('role:math, cmd:sum, left:1, right: 2', output);
