cb5d4d20c5a43e96a65b50cd2eb49fb67d5b86e3
Kenny <kt.tian@gmail.com>
2021-10-11 15:33:13 +0800

    Update math-plugin.js

--- math-plugin.js
+++ math-plugin.js
@@ -1,15 +1,22 @@
+const Seneca = require('seneca');
+const seneca = new Seneca();
+
 function math(options) {
+    this.add('role:math, cmd:sum', function (msg, resp) {
+        resp(null, {answer: msg.left + msg.right});
+    });
 
-  this.add('role:math,cmd:sum', function (msg, respond) {
-    respond(null, { answer: msg.left + msg.right })
-  })
-
-  this.add('role:math,cmd:product', function (msg, respond) {
-    respond(null, { answer: msg.left * msg.right })
-  })
-
+    this.add('role:math, cmd:product', function (msg, resp) {
+        resp(null, {answer: msg.left * msg.right});
+    });
 }
 
-require('seneca')()
-  .use(math)
-  .act('role:math,cmd:sum,left:1,right:2', console.log)
+function output(err, result) {
+    if (err) {
+        return console.error(err);
+    }
+    console.log(result);
+}
+
+
+seneca.use(math).act('role:math, cmd:sum, left:1, right: 2', output);
