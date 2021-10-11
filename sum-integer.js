41c7168addb19332d00275fc2d3e2485097308df
Kenny <kt.tian@gmail.com>
2021-10-11 14:17:17 +0800

    Update sum-integer.js

--- sum-integer.js
+++ sum-integer.js
@@ -1,20 +1,16 @@
-const seneca = require('seneca')();
+const Seneca = require('seneca');
+const seneca = new Seneca();
 
-seneca
-  .add('role:math, cmd:sum', (msg, reply) => {
-    reply(null, {
-      answer: (msg.left + msg.right)
-    })
-  })
-  .act({
-    role: 'math',
-    cmd: 'sum',
-    left: 1,
-    right: 2
-  }, console.log)
-  .act({
-    role: 'math',
-    cmd: 'sum',
-    left: 1.5,
-    right: 2.5
-  }, console.log)
+seneca.add('role:math, cmd:sum', (msg, resp) => {
+    resp(null, {answer: msg.left + msg.right});
+}).act({role: 'math', cmd: 'sum', left: 1, right: 2}, (err, result) => {
+    if (err) {
+        return console.error(err);
+    }
+    console.log('1 + 2 = ', result);
+}).act({role: 'math', cmd: 'sum', left: 1.5, right: 2.5}, (err, result) => {
+    if (err) {
+        return console.error(err);
+    }
+    console.log('1.5 + 2.5 = ', result);
+});
