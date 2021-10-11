09af0a81fcab83a72d18a5f9cc2997744067dbec
Kenny <kt.tian@gmail.com>
2021-10-11 14:21:16 +0800

    Update product.js

--- product.js
+++ product.js
@@ -1,17 +1,13 @@
-const seneca = require('seneca')();
+const Seneca = require('seneca');
+const seneca = new Seneca();
 
-seneca.add('role:math, cmd:product', (msg, reply) => {
-  reply(null, { answer: ( msg.left * msg.right )})
+seneca.add('role:math, cmd:product', (msg, resp) => {
+    resp(null, {answer: msg.left * msg.right});
 });
 
-seneca.act({
-  role: 'math',
-  cmd: 'product',
-  left: 3,
-  right: 4
-}, (err, result) => {
-  if (err) {
-    return console.error(err);
-  }
-  console.log(result);
+seneca.act({role: 'math', cmd: 'product', left: 3, right: 4}, (err, result) => {
+    if (err) {
+        return console.error(err);
+    }
+    console.log(result);
 });
