b4281d5d7df162073a8c93473b3d0b17785ac93f
Kenny <kt.tian@gmail.com>
2021-10-11 11:32:17 +0800

    Update sum-reuse.js

--- sum-reuse.js
+++ sum-reuse.js
@@ -1,22 +1,18 @@
-const seneca = require('seneca')()
+const Seneca = require('seneca');
+const seneca = new Seneca();
 
-seneca.add('role: math, cmd: sum', function (msg, respond) {
-  var sum = msg.left + msg.right
-  respond(null, {answer: sum})
-})
+seneca.add('role:math, cmd:sum', (msg, resp) => {
+    const answer = msg.left + msg.right;
+    resp(null, {answer});
+});
 
-seneca.add('role: math, cmd: sum, integer: true', function (msg, respond) {
-  // 复用 role:math, cmd:sum
-  this.act({
-    role: 'math',
-    cmd: 'sum',
-    left: Math.floor(msg.left),
-    right: Math.floor(msg.right)
-  }, respond)
-})
+seneca.add('role:math, cmd:sum, integer:true', function (msg, resp) {
+    // 复用 role:math, cmd:sum
+    this.act({role: 'math', cmd: 'sum', left: Math.floor(msg.left), right: Math.floor(msg.right)}, resp);
+});
 
 // 匹配 role:math,cmd:sum
-seneca.act('role: math, cmd: sum, left: 1.5, right: 2.5',console.log)
+seneca.act('role: math, cmd: sum, left: 1.5, right: 2.5', console.log)
 
 // 匹配 role:math,cmd:sum,integer:true
 seneca.act('role: math, cmd: sum, left: 1.5, right: 2.5, integer: true', console.log)
