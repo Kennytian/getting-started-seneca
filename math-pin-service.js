394388a78c82dba276b26fe2ced3adbafbc2c359
Kenny <kt.tian@gmail.com>
2021-10-11 16:42:44 +0800

    Update math-pin-service.js

--- math-pin-service.js
+++ math-pin-service.js
@@ -1,7 +1,6 @@
-require('seneca')()
+const Seneca = require('seneca');
+const seneca = new Seneca();
 
-  .use('math')
-
-  // 监听 role:math 消息
-  // 重要：必须匹配客户端
-  .listen({ type: 'tcp', pin: 'role:math' })
+// 监听 role:math 消息
+// 重要：必须匹配客户端
+seneca.use('math').listen({type: 'tcp', pin: 'role:math'});
