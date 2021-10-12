dad7f5680f53071fab1ec815b5f7ad56f418ca9c
Kenny <kt.tian@gmail.com>
2021-10-12 12:02:57 +0800

    Update book-store-stats.js

--- book-store-stats.js
+++ book-store-stats.js
@@ -1,14 +1,12 @@
 const stats = {};
 
-require('seneca')()
-  .add('role:store,info:purchase', function(msg, respond) {
+const Seneca = require('seneca');
+const seneca = new Seneca();
+
+seneca.add('role:store, info:purchase', (msg, reply) => {
     const id = msg.purchase.bookId;
     stats[id] = stats[id] || 0;
     stats[id]++;
     console.log(stats);
-    respond();
-  })
-  .listen({
-    port: 9003,
-    pin: 'role:store,info:purchase'
-  });
+    reply();
+}).listen({port: 9003, pin: 'role:store, info:purchase'});
